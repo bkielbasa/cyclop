55a576fe077f98706102725292c83f28dba0150b
Bartłomiej Kiełbasa <bartlomiej.kielbasa@brainly.com>
2020-05-12 13:20:14 +0200

    remove unused code
    Signed-off-by: Bartłomiej Kiełbasa <bartlomiej.kielbasa@brainly.com>

--- fmt.go
+++ fmt.go
@@ -7,25 +7,12 @@ func display(stats []stat) {
 		fmt.Printf("%.3g\n", average(stats))
 		return
 	}
-	if *top == 0 {
-		displayStats(stats)
-	} else {
-		displayTopStats(stats, *top)
-	}
+
+	displayStats(stats)
 }
 
 func displayStats(stats []stat) {
 	for _, s := range stats {
-		fmt.Printf("%d %s.%s\n", s.Complexity, s.Pkg, s.FuncName)
-	}
-}
-
-func displayTopStats(stats []stat, top int) {
-	for i, s := range stats {
-		if i == top {
-			return
-		}
-
 		fmt.Printf("%d %s.%s\n", s.Complexity, s.Pkg, s.FuncName)
 	}
 }

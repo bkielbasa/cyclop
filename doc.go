9bd5cb023bcc84b781e323975921a7e362e12a2b
Bartłomiej Kiełbasa <bartlomiej.kielbasa@brainly.com>
2020-05-20 12:42:29 +0200

    add doc.go

---
+++ doc.go
@@ -0,0 +1,7 @@
+// Cyclop calculates cyclomatic complexities of functions in Go source code.
+// Simple usage:
+//
+//     cyclop .
+//
+// Read more about usage in https://github.com/bkielbasa/cyclop/blob/master/README.md
+package main
